dabea6be9ea515fcbbe0089b20c7cd40f3aed2c8
Nahid-07 <nahidasif1998@gmail.com>
2025-09-02 09:07:10 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+import express from "express";
+import cors from "cors";
+import { MongoClient, ObjectId } from "mongodb";
+const app = express();
+const port = process.env.PORT || 5000;
+import { configDotenv } from "dotenv";
+configDotenv()
+const uri =
+  `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.ugpmzsn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri);
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("artizans server is running");
+});
+
+async function run() {
+  try {
+    const productData = client.db("artizans-mart").collection("product-data");
+    const productReviews = client.db("artizans-mart").collection("reviews");
+    const placeOrder = client.db("artizans-mart").collection("orders");
+
+    app.post("/addProduct", async (req, res) => {
+      const body = await req.body;
+      const result = await productData.insertOne(body);
+      res.send(result);
+    });
+
+    app.get("/products", async (req, res) => {
+      const data = await productData.find().toArray();
+      res.send(data);
+    });
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const data = await productData.findOne(query);
+      const reviewOfProductCount = await productReviews.find().toArray();
+      const filterRiview = reviewOfProductCount.filter(i => i.productId ===  id)
+      res.send({data, filterRiview});
+    });
+
+    // product reviews post api
+    app.post("/reviews", async (req, res) => {
+      const body = req.body;
+      const reviews = await productReviews.insertOne(body);
+      res.send(reviews);
+    });
+
+    app.get("/reviews", async (req, res) => {
+      const data = await productReviews.find().toArray();
+      res.send(data);
+    });
+
+    // order details post operation 
+    app.post('/place-order', async(req,res)=>{
+      const body = req.body;
+      const orders = await placeOrder.insertOne(body);
+      res.send(orders);
+    })
+  } finally {
+  }
+}
+run().catch((err) => {
+  console.log(err);
+});
+app.listen(port);
