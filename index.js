fd4412322f6941e8fa05ac02570ed50c07ba11bc
Nahid-07 <nahidasif1998@gmail.com>
2025-12-05 18:11:38 +0600

    fixed cors bug

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://artizans-mart-ecommerce-project.web.app",
-      "https://artizans-mart-ecommerce-project.firebaseapp.com",
+      "https://artizans-mart-auth.web.app/",
     ],
     credentials: true,
   })
