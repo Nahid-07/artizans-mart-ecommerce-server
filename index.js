13e188543f4eefe2d62e4025f79a51342d3e42c6
Nahid-07 <nahidasif1998@gmail.com>
2026-05-10 16:03:02 +0600

    added local url

--- index.js
+++ index.js
@@ -12,11 +12,22 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "http://localhost:5174",
+//       "https://artizans-mart-auth.web.app",
+//     ],
+//     credentials: true,
+//   })
+// );
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      "http://localhost:5173", 
       "http://localhost:5174",
+      "http://localhost:3000", // <-- ADD YOUR NEXT.JS PORT HERE
       "https://artizans-mart-auth.web.app",
     ],
     credentials: true,
